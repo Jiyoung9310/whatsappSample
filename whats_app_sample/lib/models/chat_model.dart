462e7ff635415f0447b54660175c4d15a511b071
Jungee <jungee9732@osstem.com>
2019-01-07 16:38:32 +0900

    채팅방 리스트 만들기 - 끝

---
+++ whats_app_sample/lib/models/chat_model.dart
@@ -0,0 +1,84 @@
+class ChatModel{
+  final String name;
+  final String message;
+  final String time;
+  final String avatarUrl;
+
+  ChatModel({this.name, this.message, this.time, this.avatarUrl});
+
+
+}
+List<ChatModel> dummyData = [
+  new ChatModel(
+      name: "Pawan Kumar",
+      message: "Hey Flutter, You are so amazing!",
+      time: "15:30",
+      avatarUrl: "https://img2.nickiswift.com/img/gallery/celeb-reactions-to-time-person-of-the-year/ellen-degeneres-1512671685.jpg"
+  ),
+  new ChatModel(
+      name: "Harvey Spectre",
+      message: "Hey I ahve hacked whatsapp",
+      time: "17:30",
+      avatarUrl: "https://img2.nickiswift.com/img/gallery/celeb-reactions-to-time-person-of-the-year/ellen-degeneres-1512671685.jpg"
+  ),
+  new ChatModel(
+      name: "Mike Ross",
+      message: "Wassup !",
+      time: "5:00",
+      avatarUrl: "https://img2.nickiswift.com/img/gallery/celeb-reactions-to-time-person-of-the-year/ellen-degeneres-1512671685.jpg"
+  ),
+  new ChatModel(
+      name: "Rachel",
+      message: "I'm good!",
+      time: "10:30",
+      avatarUrl: "https://img2.nickiswift.com/img/gallery/celeb-reactions-to-time-person-of-the-year/ellen-degeneres-1512671685.jpg"
+  ),
+  new ChatModel(
+      name: "Barry Allen",
+      message: "I'm the fastest man alive!",
+      time: "12:30",
+      avatarUrl: "https://img2.nickiswift.com/img/gallery/celeb-reactions-to-time-person-of-the-year/ellen-degeneres-1512671685.jpg"
+  ),
+  new ChatModel(
+      name: "Joe West",
+      message: "This is so cool!",
+      time: "19:22",
+      avatarUrl: "https://img2.nickiswift.com/img/gallery/celeb-reactions-to-time-person-of-the-year/ellen-degeneres-1512671685.jpg"
+  ),
+  new ChatModel(
+      name: "Pawan Kumar2",
+      message: "Hey Flutter, You are so amazing!",
+      time: "12:30",
+      avatarUrl: "https://img2.nickiswift.com/img/gallery/celeb-reactions-to-time-person-of-the-year/ellen-degeneres-1512671685.jpg"
+  ),
+  new ChatModel(
+      name: "Harvey Spectre2",
+      message: "Hey I ahve hacked whatsapp",
+      time: "20:30",
+      avatarUrl: "https://img2.nickiswift.com/img/gallery/celeb-reactions-to-time-person-of-the-year/ellen-degeneres-1512671685.jpg"
+  ),
+  new ChatModel(
+      name: "Mike Ross2",
+      message: "Wassup !",
+      time: "19:00",
+      avatarUrl: "https://img2.nickiswift.com/img/gallery/celeb-reactions-to-time-person-of-the-year/ellen-degeneres-1512671685.jpg"
+  ),
+  new ChatModel(
+      name: "Rachel2",
+      message: "I'm good!",
+      time: "10:30",
+      avatarUrl: "https://img2.nickiswift.com/img/gallery/celeb-reactions-to-time-person-of-the-year/ellen-degeneres-1512671685.jpg"
+  ),
+  new ChatModel(
+      name: "Barry Allen2",
+      message: "I'm the fastest man alive!",
+      time: "12:30",
+      avatarUrl: "https://img2.nickiswift.com/img/gallery/celeb-reactions-to-time-person-of-the-year/ellen-degeneres-1512671685.jpg"
+  ),
+  new ChatModel(
+      name: "Joe West2",
+      message: "This is so cool!",
+      time: "19:22",
+      avatarUrl: "https://img2.nickiswift.com/img/gallery/celeb-reactions-to-time-person-of-the-year/ellen-degeneres-1512671685.jpg"
+  ),
+];
